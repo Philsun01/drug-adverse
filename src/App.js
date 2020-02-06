ecde9674ad588b57895278fd721056cb39713eee
Philip Sun <philipsun@Philips-MacBook-Pro.local>
2020-02-06 15:27:37 -0800

    made general API call for 5 entries at a time

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const API = 'https://api.fda.gov/drug/event.json';
+  const [data, setData] = useState([]);
+  
+  const fetchData = ()=> {
+    const field = 'reactionmeddrapt';
+    const limit = 5;
+    const url = `${API}?limit=${limit}`;
+    axios.get(url)
+      .then(res => {
+        setData(res.data.results);
+        console.log(res.data.results);
+      })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Practice calling API</h1>
+      <button onClick = {()=> fetchData()}>Fetch Data</button>
+      {
+        data.map( event => {
+          return (
+            <div key = {event.safetyreportid}>
+              Drug Name: {event.patient.drug[0].medicinalproduct}
+              <br />
+              Patient Age: {event.patient.patientonsetage}
+                <ul>
+                {
+                  event.patient.reaction.map( (reaction, idx) => {
+                    return (
+                      <li key = {idx}>
+                        {reaction.reactionmeddrapt}
+                      </li>
+                    )
+                  } )
+                }
+                </ul>
+            </div>
+          )
+        })
+      }
     </div>
   );
 }
